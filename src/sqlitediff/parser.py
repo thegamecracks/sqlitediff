326ac8034b130e02889d700277f41ced500887b6
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-12-12 11:29:38 -0500

    perf: cache table grammar for subsequent calls

--- src/sqlitediff/parser.py
+++ src/sqlitediff/parser.py
@@ -1,8 +1,14 @@
+import functools
 import importlib.resources
 
 import lark
 
 
-def create_table_parser() -> lark.Lark:
-    grammar = importlib.resources.read_text(__package__, "table.lark")
-    return lark.Lark(grammar)
+@functools.lru_cache
+def get_table_grammar() -> str:
+    return importlib.resources.read_text(__package__, "table.lark")
+
+
+def create_table_parser(*args, **kwargs) -> lark.Lark:
+    grammar = get_table_grammar()
+    return lark.Lark(grammar, *args, **kwargs)
