9fa178053633a49b8c08d97df9f0ee5a8c5e15ec
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-12-12 12:03:17 -0500

    feat: add table transformer and tests

--- src/sqlitediff/parser.py
+++ src/sqlitediff/parser.py
@@ -12,3 +12,51 @@ def get_table_grammar() -> str:
 def create_table_parser(*args, **kwargs) -> lark.Lark:
     grammar = get_table_grammar()
     return lark.Lark(grammar, *args, **kwargs)
+
+
+class TableTransformer(lark.Transformer):
+    def name(self, children):
+        return str(children[0])
+
+    def type(self, children):
+        return str(children[0])
+
+    def unknown_token(self, children):
+        return " ".join(children)
+
+    def column_constraint(self, children):
+        return " ".join(children)
+
+    def column(self, children):
+        from .schema import Column
+        if len(children) < 2: children.append(None)
+        return Column(name=children[0], type=children[1], constraints=set(children[2:]))
+
+    def columns(self, children):
+        return set(children)
+
+    def table_constraint(self, children):
+        return " ".join(children)
+
+    def table_constraints(self, children):
+        return set(children)
+
+    def table_option(self, children):
+        return " ".join(children)
+
+    def table_options(self, children):
+        return set(children)
+
+    def create_table(self, children):
+        from .schema import Table
+        if len(children) < 3: children.append(set())
+        if len(children) < 4: children.append(set())
+        return Table(
+            name=children[0],
+            columns=children[1],
+            constraints=children[2],
+            options=children[3],
+        )
+
+    def statement(self, children):
+        return children[0]
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import lark
 import pytest
 
-from sqlitediff.parser import create_table_parser
+from sqlitediff.parser import TableTransformer, create_table_parser
 
 
 @pytest.fixture(scope="session")
@@ -15,53 +15,86 @@ def test_one_column(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (x)")
     print(tree.pretty())
 
+    tables = TableTransformer().transform(tree).children
+    print(tables)
+
 
 def test_two_columns(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (x, y)")
     print(tree.pretty())
+
+    tables = TableTransformer().transform(tree).children
+    print(tables)
 
 
 def test_typed_columns(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (x INT, y INT)")
     print(tree.pretty())
 
+    tables = TableTransformer().transform(tree).children
+    print(tables)
+
 
 def test_column_constraint(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (id INTEGER PRIMARY KEY)")
     print(tree.pretty())
+
+    tables = TableTransformer().transform(tree).children
+    print(tables)
 
 
 def test_multi_column_constraint(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (id INTEGER PRIMARY KEY, bar TEXT NOT NULL)")
     print(tree.pretty())
 
+    tables = TableTransformer().transform(tree).children
+    print(tables)
+
 
 def test_multi_constraint_column(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (bar TEXT UNIQUE NOT NULL)")
     print(tree.pretty())
+
+    tables = TableTransformer().transform(tree).children
+    print(tables)
 
 
 def test_table_constraint(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (id, PRIMARY KEY (id))")
     print(tree.pretty())
 
+    tables = TableTransformer().transform(tree).children
+    print(tables)
+
 
 def test_table_and_column_constraint(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (x INTEGER PRIMARY KEY, FOREIGN KEY (id) REFERENCES bar (x))")
     print(tree.pretty())
+
+    tables = TableTransformer().transform(tree).children
+    print(tables)
 
 
 def test_table_option(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (x INTEGER) STRICT")
     print(tree.pretty())
 
+    tables = TableTransformer().transform(tree).children
+    print(tables)
+
 
 def test_table_options(table_parser: lark.Lark) -> None:
     tree = table_parser.parse("CREATE TABLE foo (x TEXT PRIMARY KEY) STRICT, WITHOUT ROWID")
     print(tree.pretty())
+
+    tables = TableTransformer().transform(tree).children
+    print(tables)
 
 
 def test_image_tags(table_parser: lark.Lark) -> None:
     path = Path(__file__).parent / "image_tags.sql"
     tree = table_parser.parse(path.read_text())
     print(tree.pretty())
+
+    tables = TableTransformer().transform(tree).children
+    print(tables)
