df5fdb4eaffcb09dddfb977b289ffeec5b211690
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-12-12 20:23:56 -0500

    feat: export dataclasses and functions

--- src/sqlitediff/__init__.py
+++ src/sqlitediff/__init__.py
@@ -2,4 +2,32 @@
 Compares the schemas of two SQLite databases and generates
 a simplified SQL script for aiding migrations.
 """
+from .diff import (
+    Change,
+    DeletedColumn,
+    DeletedObject,
+    DeletedTable,
+    ModifiedColumn,
+    ModifiedObject,
+    ModifiedTable,
+    NewColumn,
+    NewObject,
+    NewTable,
+    ObjectType,
+    SchemaDiff,
+    schema_diff,
+)
+from .schema import (
+    Column,
+    ColumnOption,
+    Index,
+    Schema,
+    Table,
+    TableConstraint,
+    TableOption,
+    Trigger,
+    View,
+    load_schema,
+)
+
 __version__ = "0.1.0"
