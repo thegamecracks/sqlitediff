b4294e381c679e24908bf6846dc6e39652b79511
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-12-12 20:39:03 -0500

    fix: incorrect SQL script in test_load_user_group_2

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -15,7 +15,7 @@ def test_load_user_group_1():
 
 
 def test_load_user_group_2():
-    path = Path(__file__).parent / "user_group_1.sql"
+    path = Path(__file__).parent / "user_group_2.sql"
     with contextlib.closing(sqlite3.connect(":memory:")) as conn:
         conn.executescript(path.read_text())
         schema = load_schema(conn)
